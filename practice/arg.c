f5c6313a173dd7b6aefd245dd2b2f7344230d925
ZahoorOnly <i.am.zahoor.a@outlook.com>
2023-10-12 21:31:51 +0500

    arguments like flags in commands learnt

---
+++ practice/arg.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#define name "Faizan"
+int main(int arguments, char *abc[]){
+    printf("arg");
+    printf("File name is: %s\n",abc[0]);
+    printf("Argument Counter: %d\n",arguments);
+    printf("Constant Name: %s\n",name);
+}
