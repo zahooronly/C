5b3f8225e3f459045b0d59fc9269679e13eee99e
ZahoorOnly <i.am.zahoor.a@outlook.com>
2023-10-29 14:19:06 +0500

    table in c

---
+++ practice/hassan.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main(){
+    // int a=10;
+    // int b=20;
+    // int c=30;
+    // int d=40;
+    // unsigned int a,b,c,d;
+    // printf("Hello World\n");
+    // scanf("%d %d %d %d",&a,&b,&c,&d);
+    // printf("Values are: %d %i %d %i\n",a,c,b,d);
+    int length,table;
+    printf("Enter the length of table: ");
+    scanf("%d",&length);
+    printf("Enter table number: ");
+    scanf("%d",&table);
+    for(int i=1;i<=length;i++){
+        printf("%d * %d = %d\n",table,i,table*i);
+    }
+}
